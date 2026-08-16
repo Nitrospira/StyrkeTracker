7619f8f641b5effceb3a83e0e2e87531387edca4
Nitrospira <larsen1104@gmail.com>
2026-08-16 10:38:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE="styrketracker-v1";
-const ASSETS=["./","./index.html","./manifest.json"];
+const CACHE="styrketracker-v2";
+const ASSETS=["./", "./index.html", "./manifest.json", "./benpres.png", "./chest-press.png", "./lat-pulldown.png", "./leg-curl.png", "./seated-row.png", "./shoulder-press.png", "./cable-crunch.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
